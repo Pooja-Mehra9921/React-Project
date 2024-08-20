f566f51a0506d017ca0d35a10c5758ef34eaadcc
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2024-08-20 16:35:12 +0530

    making caraousel

---
+++ amazon-clone/src/components/Banner/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Sliders from "../../data/carouselData.json"
+import { Box } from "@mui/material";
+
+const Banner =()=>{
+const Images =[
+    {
+
+    }
+]
+
+    return(
+        <>
+        <Box>
+            {
+
+            }
+        </Box>
+        </>
+    );
+};
+
+export default Banner;
--- amazon-clone/src/pages/Homepage/index.jsx
+++ amazon-clone/src/pages/Homepage/index.jsx
@@ -4,12 +4,15 @@ import SubHeader from "../../components/Sub-header"
 import Footer from "../../components/Footer";
 import "./style.css"
 import FooterBasic from "../../components/FooterBasic";
+import Banner from "../../components/Banner";
 
 const Homepage=()=>{
 return(
     <>
+    
     <Header/>
     <SubHeader/>
+    <Banner/>
     <Footer/>
     
     <FooterBasic/>
