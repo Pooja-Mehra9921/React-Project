92a8d941d5244f0259857eddaad2cbc0cce6d2f1
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2024-08-22 18:34:51 +0530

    making carousel

--- amazon-clone/src/components/Banner/index.jsx
+++ amazon-clone/src/components/Banner/index.jsx
@@ -1,21 +1,42 @@
 import React from "react";
-import Sliders from "../../data/carouselData.json"
 import { Box } from "@mui/material";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import image1 from "../../assets/images/caraousel-images/carousel-image1.jpg"
+import image2 from "../../assets/images/caraousel-images/carousel-image2.jpg"
+import image3 from "../../assets/images/caraousel-images/carousel-image3.jpg"
+import image4 from "../../assets/images/caraousel-images/carousel-image4.jpg"
+import image5 from "../../assets/images/caraousel-images/carousel-image5.jpg"
+import image6 from "../../assets/images/caraousel-images/carousel-image6.jpg"
+import image7 from "../../assets/images/caraousel-images/carousel-image7.jpg"
 
-const Banner =()=>{
-const Images =[
-    {
-
-    }
-]
+const Banner =()=>{;
 
     return(
         <>
-        <Box>
-            {
-
-            }
-        </Box>
+       <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true}>
+                <div>
+                    <img src={image1} />
+                </div>
+                <div>
+                    <img src={image2} />
+                </div>
+                <div>
+                    <img src={image3} />
+                </div>
+                <div>
+                    <img src={image4} />
+                </div>
+                <div>
+                    <img src={image5} />
+                </div>
+                <div>
+                    <img src={image6} />
+                </div>
+                <div>
+                    <img src={image7} />
+                </div>
+            </Carousel>
         </>
     );
 };
